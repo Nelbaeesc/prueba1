4e605b5c2999a9886e38cc358a64575c775d1e4e
Nelson Baez <nelbaeesc@alu.edu.gva.es>
2023-02-09 17:27:10 +0100

    a

---
+++ prueba1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nbaeze
+ *
+ */
+module prueba1 {
+}
---
+++ prueba1/src/prueba1/prueba1.java
@@ -0,0 +1,13 @@
+package prueba1;
+
+public class prueba1 {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo java-git");
+	}
+}
